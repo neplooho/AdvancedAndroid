3dd06cbf177f42ff3f7cabda5517b52bec14608e
Eugene <zhek0nd98@gmail.com>
2018-05-19 21:05:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/eugene/advancedandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.eugene.advancedandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
