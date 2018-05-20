d1a88fd085ed1c52295dd65752cb2ec0b0e766a6
Eugene <zhek0nd98@gmail.com>
2018-05-20 11:09:48 +0300

    Creating activity scope

--- app/src/main/java/com/example/eugene/advancedandroid/home/MainActivity.java
+++ app/src/main/java/com/example/eugene/advancedandroid/home/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.eugene.advancedandroid;
+package com.example.eugene.advancedandroid.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.eugene.advancedandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/eugene/advancedandroid/home/MainActivityComponent.java
@@ -0,0 +1,16 @@
+package com.example.eugene.advancedandroid.home;
+
+import com.example.eugene.advancedandroid.di.ActivityScope;
+
+import dagger.Subcomponent;
+import dagger.android.AndroidInjector;
+
+@ActivityScope
+@Subcomponent
+public interface MainActivityComponent extends AndroidInjector<MainActivity> {
+
+    @Subcomponent.Builder
+    abstract class Builder extends AndroidInjector.Builder<MainActivity>{
+
+    }
+}
