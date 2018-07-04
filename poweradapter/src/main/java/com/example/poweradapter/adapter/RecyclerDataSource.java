96b36d8714305d4bca5a9316a970ac60c2f14c6f
Eugene <zhek0nd98@gmail.com>
2018-07-04 16:44:20 +0300

    Made a RecyclerDataSource for custom module

---
+++ poweradapter/src/main/java/com/example/poweradapter/adapter/RecyclerDataSource.java
@@ -0,0 +1,66 @@
+package com.example.poweradapter.adapter;
+
+import android.annotation.SuppressLint;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.MainThread;
+import android.support.v7.util.DiffUtil;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.poweradapter.item.ItemRenderer;
+import com.example.poweradapter.item.RecyclerItem;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RecyclerDataSource {
+
+    private final Map<String, ItemRenderer<? extends RecyclerItem>> renderers;
+    @SuppressLint("UseSparseArrays")
+    private final Map<Integer, String> viewTypeToRenderKeyMap = new HashMap<>();
+    private final List<RecyclerItem> data = new ArrayList<>();
+
+    private WeakReference<RecyclerView.Adapter> adapterReference = new WeakReference<>(null);
+
+    public RecyclerDataSource(Map<String, ItemRenderer<? extends RecyclerItem>> renderers) {
+        this.renderers = renderers;
+        for (Map.Entry<String, ItemRenderer<? extends RecyclerItem>> entry : renderers.entrySet()) {
+            viewTypeToRenderKeyMap.put(entry.getValue().layoutRes(), entry.getKey());
+        }
+    }
+
+    @MainThread
+    public void setData(List<? extends RecyclerItem> newData) {
+        DiffUtil.DiffResult diffResult = DiffUtil.calculateDiff(new RecyclerDiffCallback(data, newData));
+        data.clear();
+        data.addAll(newData);
+        RecyclerView.Adapter adapter = adapterReference.get();
+        if (adapter != null) {
+            diffResult.dispatchUpdatesTo(adapter);
+        }
+    }
+
+    ItemRenderer<RecyclerItem> rendererForType(int viewType) {
+        //noinspection unchecked
+        return (ItemRenderer<RecyclerItem>) renderers.get(viewTypeToRenderKeyMap.get(viewType));
+    }
+
+    @LayoutRes
+    int viewResourceForPosition(int position) {
+        return renderers.get(data.get(position).renderKey()).layoutRes();
+    }
+
+    int getCount() {
+        return data.size();
+    }
+
+    RecyclerItem getItem(int position) {
+        return data.get(position);
+    }
+
+    void attachToAdapter(RecyclerView.Adapter adapter) {
+        adapterReference = new WeakReference<RecyclerView.Adapter>(adapter);
+    }
+}
---
+++ poweradapter/src/main/java/com/example/poweradapter/adapter/RecyclerDiffCallback.java
@@ -0,0 +1,37 @@
+package com.example.poweradapter.adapter;
+
+import android.support.v7.util.DiffUtil;
+
+import com.example.poweradapter.item.RecyclerItem;
+
+import java.util.List;
+
+public class RecyclerDiffCallback extends DiffUtil.Callback {
+
+    private final List<? extends RecyclerItem> oldList;
+    private final List<? extends RecyclerItem> newList;
+
+    RecyclerDiffCallback(List<? extends RecyclerItem> oldList, List<? extends RecyclerItem> newList) {
+        this.oldList = oldList;
+        this.newList = newList;
+    }
+    @Override
+    public int getOldListSize() {
+        return oldList.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+        return newList.size();
+    }
+
+    @Override
+    public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+        return oldList.get(oldItemPosition).getId() == newList.get(newItemPosition).getId();
+    }
+
+    @Override
+    public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+        return oldList.get(oldItemPosition).equals(newList.get(newItemPosition));
+    }
+}
